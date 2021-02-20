50cad065d839e2bd554e333a1f260a36bc40e806
stas <stas.v.nebyshinets@gmail.com>
2021-02-21 00:58:59 +0300

    feat: add initial features

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let starts = [];
+  let ends = [];
+  bracketsConfig.forEach( (element) => {
+    starts.push(element[0]);
+    ends.push(element[1]);
+  })
+  
+  const len = str.length;
+  let buf = [];
+  for(let i = 0; i < len; i++){
+    const char = str.charAt(i);
+    let startIndex = starts.indexOf(char);
+    if( startIndex < 0 ){
+      startIndex = buf.pop();
+      if( ends[startIndex] != char ){
+        return false;
+      } 
+    } else {
+      if(char == ends[startIndex]){
+        if(starts[buf[buf.length-1]] == char){
+          buf.pop();
+          continue;
+        } else {
+          buf.push(startIndex);
+          continue;
+        }
+      }
+      buf.push(startIndex);
+    }
+  }
+  if(buf.length != 0){
+    return false;
+  }
+  return true;
 }
